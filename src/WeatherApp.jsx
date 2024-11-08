197ff9e236c7f6d8e3aa1bfbf6f9428e483a888b
faizanKhan <faizankhan46849@gmail.com>
2024-11-08 16:14:32 +0530

    intial app

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,14 @@
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+export default function () {
+    return (
+        <div className='flex justify-center items-center h-[100vh] w-[100vw]'>
+          <Container className='flex justify-center'>
+            <Box sx={{ bgcolor: '#cfe8fc', height: '500px', width: "400px" }} >
+               
+            </Box>
+          </Container>
+        </div>
+    )
+}
