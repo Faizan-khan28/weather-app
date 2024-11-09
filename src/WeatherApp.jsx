c63a83f21b1bcf6d823a0a62ef2803178df2b066
faizanKhan <faizankhan46849@gmail.com>
2024-11-09 15:30:02 +0530

    add functions handlechange and submit

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,32 +1,63 @@
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 export default function () {
-    return (
-        <div className='flex justify-center items-center h-[100vh] w-[100vw] '>
-          <Container className='flex justify-center'>
-            <Box sx={{ bgcolor: '#cfe8fc', height: '500px', width: "400px" }} className="flex items-center gap-9 rounded-md  flex-col" >
-               <h1 className='font-medium text-[40px]'>Weather App</h1>
-               <div>
-                <TextField style={{width: "300px"}} label="Enter City Name" variant="outlined" />
-               </div>
-               <div>
-                <Button style={{background: "black", width: "150px"}} variant="contained">Search</Button>
-                </div>
-              
-               <div className='text-[18px] font-medium'>
-                 <h1>Delhi</h1>
-                 <p>temp:</p>
-                 <p>Humidity:</p>
-                 <p>Min-Tem:</p>
-                 <p>Max-Tem:</p>
-                 <p>Feels Like cold</p>
-               </div>
-            </Box>
-          </Container>
-        </div>
-    )
+
+  let [city, setCity] = useState("")
+
+  let handleChange = (event) => {
+    setCity(event.target.value);
+  }
+
+  let submit = (event) => {
+    event.preventDefault();
+    setCity("")
+    console.log(city);
+  }
+
+  return (
+    <div className="flex justify-center items-center h-[100vh] w-[100vw] ">
+      <Container className="flex justify-center">
+        <Box
+          sx={{ bgcolor: "#cfe8fc", height: "500px", width: "400px" }}
+          className="flex items-center gap-9 rounded-md  flex-col"
+        >
+          <h1 className="font-medium text-[40px]">Weather App</h1>
+          <form onSubmit={submit}>
+            <div>
+              <TextField
+                style={{ width: "300px" }}
+                label="Enter City Name"
+                variant="outlined"
+                required
+                value={city}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="text-center mt-5">
+              <Button
+                type="submit"
+                style={{ background: "black", width: "150px" }}
+                variant="contained"
+              >
+                Search
+              </Button>
+            </div>
+          </form>
+
+          <div className="text-[18px] font-medium">
+            <h1 className="text-[40px] text-center">Delhi</h1>
+            <p>temp:</p>
+            <p>Humidity:</p>
+            <p>Min-Tem:</p>
+            <p>Max-Tem:</p>
+            <p>Feels Like cold</p>
+          </div>
+        </Box>
+      </Container>
+    </div>
+  );
 }
